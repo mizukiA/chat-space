b2bcd79e844af203dac3c511876d37e59866d321
Mizuki Ando <mzk5326@gmail.com>
2019-05-23 12:50:50 +0900

    test controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/support/**/*.rb')].each{|f| require f}
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -60,3 +63,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+require 'devise'
+require File.expand_path("spec/support/controller_macros.rb")
