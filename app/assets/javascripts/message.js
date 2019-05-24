d521b1009cce8b30b2515afc801a277642bd0c17
Mizuki Ando <mzk5326@gmail.com>
2019-05-24 17:21:54 +0900

    create html in .js file

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
-$('.js-form').on('submit', function(){
+function buildHTML(message){
+  if ( message.image ) {
+    var html =
+     `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <asset_path src=${message.image} >
+      </div>`
+    return html;
+  } else {
+    var html =
+     `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    return html;
+  };
+}
+$('.form__message').on('submit', function(){
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action')
@@ -9,5 +49,8 @@ $('.js-form').on('submit', function(){
     dataType: 'json',
     processData: false,
     contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
   })
 });
