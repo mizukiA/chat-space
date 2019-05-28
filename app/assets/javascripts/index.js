7d4483b115a10d9cb4eda6bf876b9d6d04dbacc9
Mizuki Ando <mzk5326@gmail.com>
2019-05-28 21:13:11 +0900

    Update index

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,13 +1,13 @@
 $(function() {
   
-  var search_list=$("#user-search-result")
-  var member_list=$(".chat-group-user__name")
+  var search_list = $("#user-search-result")
+  var member_list = $(".chat-group-user__name")
 
   function appendUser(user) {
      var html = 
-   `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${user.name}</p>
-    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name=${user.name}>追加
+   `<div class = "chat-group-user clearfix">
+    <p class = "chat-group-user__name">${user.name}</p>
+    <div class = "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name=${user.name}>追加
     </div>
    </div>`
    search_list.append(html);
@@ -16,17 +16,17 @@ $(function() {
   function appendErr(user){
     var html = 
     `<li>
-    <div class='#user-search-field.chat-group-form__input'>${ user }</div>
+    <div class ='#user-search-field.chat-group-form__input'>${ user }</div>
     </li>`
     search_list.append(html);
   }
 
   function appendMember(name,user_id){
     var html = 
-    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-      <p class='chat-group-user__name'>${name}</p>
-      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+    `<div class ='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <input name ='group[user_ids][]' type ='hidden' value ='${user_id}'>
+      <p class ='chat-group-user__name'>${name}</p>
+      <div class ='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
     </div>`
     member_list.append(html);
   }
@@ -57,8 +57,8 @@ $(function() {
       })
       $(function() {
         $(document).on('click', '.user-search-add',function() {
-          var name =$(this).attr("data-user-name");
-          var user_id =$(this).attr("data-user-id");
+          var name = $(this).attr("data-user-name");
+          var user_id = $(this).attr("data-user-id");
           appendMember(name,user_id);
         })
         $(document).on('click','.user-search-remove',function() {
