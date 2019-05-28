39b88b4d4cf78910b6dfcf9781e500197c19f535
Mizuki Ando <mzk5326@gmail.com>
2019-05-28 17:54:43 +0900

    Update index:add click

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,7 @@
 $(function() {
+  
   var search_list=$("#user-search-result")
+  var member_list=$(".chat-group-user__name")
 
   function appendUser(user) {
      var html = 
@@ -9,37 +11,61 @@ $(function() {
     </div>
    </div>`
    search_list.append(html);
-}
-
-function appendErr(user){
-  var html = 
-   `<li>
-   <div class='#user-search-field.chat-group-form__input'>${ user }</div>
-   </li>`
-   search_list.append(html);
-}
-
-  $("#user-search-field").on("keyup", function() {
-    var input = $('#user-search-field').val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: {name: input },
-      dataType: 'json'
-})
-.done(function(users) {
-  $("#user-search-result").empty();
-  if (users.length !== 0) {
-    users.forEach(function(user){
-      appendUser(user);
-    });
   }
-  else {
-    appendErr("一致するユーザーはいません");
+
+  function appendErr(user){
+    var html = 
+    `<li>
+    <div class='#user-search-field.chat-group-form__input'>${ user }</div>
+    </li>`
+    search_list.append(html);
   }
-})
-.fail(function() {
-      alert('ユーザー検索に失敗しました');
+
+  function appendMember(name,user_id){
+    var html = 
+    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+      <p class='chat-group-user__name'>${name}</p>
+      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+    </div>`
+    member_list.append(html);
+  }
+
+  $(function() {
+    $("#user-search-field").on("keyup", function() {
+      var input = $('#user-search-field').val();
+
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {name: input },
+        dataType: 'json'
+      })
+
+      .done(function(users) {
+        search_list.empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }else {
+          appendErr("一致するユーザーはいません");
+        }
+      })
+      .fail(function() {
+            alert('ユーザー検索に失敗しました');
+      })
+      $(function() {
+        $(document).on('click', '.user-search-add',function() {
+          var name =$(this).attr("data-user-name");
+          var user_id =$(this).attr("data-user-id");
+          appendMember(name,user_id);
+        })
+        $(document).on('click','.user-search-remove',function() {
+          $(".chat-group-user__name").remove();
+          appendMember(name,user_id);
+        })
+      });
     });
   });
 });
