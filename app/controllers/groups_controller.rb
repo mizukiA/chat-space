53d9972322c7bbc1aab08e075c0a7b007fe126b1
Mizuki Ando <mzk5326@gmail.com>
2019-05-31 18:55:59 +0900

    update controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,10 +32,10 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, {user_ids: []}, current_user.id)
+    params.require(:group).permit(:name, {:user_ids =>[]})
   end
 
   def set_group
-    @group=Group.find(params[:id])
+    @group = Group.find(params[:id])
   end
 end
